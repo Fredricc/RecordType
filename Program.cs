9f40350c452c548a4b86c2126e36c4313f41a49c
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-07 08:57:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace RecordType
+{
+    /// <summary>
+    /// Example of a record type..
+    /// </summary>
+    /// <param name="FirstName"></param>
+    /// <param name="LastName"></param>
+    public record Person(string FirstName, string LastName);
+
+    public record Employee(): Person("First", "Last");
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Steve", "Bandaman");
+            Person person2 = new Person("Steve", "Bandaman");
+
+            if (person1 == person2)
+            {
+                Console.WriteLine("Persons are equal.");
+            }
+            else
+            {
+            Console.WriteLine("Hello, persons are different.");
+
+            }
+        }
+    }
+}
